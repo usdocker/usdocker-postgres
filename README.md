f8ec47cc85f4bc97cb69523e70f6970801979bde
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-08-20 13:55:28 -0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,58 @@
+# Useful script for 'postgres' service
+
+This Useful Script creates a postgres server based on a Docker Image.
+You don't have know docker to use this solution.
+
+## Installing
+
+```bash
+npm install -g usdocker-postgres
+```
+
+## Start the postgres service
+
+```bash
+usdocker postgres up
+```
+
+## Stop the postgres service
+
+```bash
+usdocker postgres down
+```
+
+## Check the postgres status
+
+```bash
+usdocker postgres status
+```
+
+## Use the `psql` command line client
+
+```bash
+usdocker postgres client -- [args]
+```
+
+## Run the bash 
+
+```bash
+usdocker postgres connect -- [args]
+```
+
+
+## Customize your service
+
+You can setup the variables by using:
+
+```bash
+usdocker postgres --set variable=value
+```
+
+Default values
+
+ - image: "postgres:9-alpine",
+ - folder: "/home/jg/.usdocker/data/postgres",
+ - user: "postgres",
+ - password: "password",
+ - port: 5432
+
---
+++ usdocker_postgres.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const usdocker = require('usdocker');
+//const path = require('path');
+
+const SCRIPTNAME = 'postgres';
+
+let config = usdocker.config(SCRIPTNAME);
+let configGlobal = usdocker.configGlobal();
+const CONTAINERNAME = SCRIPTNAME + configGlobal.get('container-suffix');
+
+function getContainerDef() {
+
+    let docker = usdocker.dockerRunWrapper(configGlobal);
+    return docker
+        .containerName(CONTAINERNAME)
+        .port(config.get('port'), 5432)
+        .volume(config.get('folder'), '/var/lib/postgresql/data')
+        .env('TZ', configGlobal.get('timezone'))
+        .env('POSTGRES_USER', config.get('user'))
+        .env('POSTGRES_PASSWORD', config.get('password'))
+        .isDetached(true)
+        .isRemove(true)
+        .imageName(config.get('image'))
+    ;
+}
+
+module.exports = {
+    setup: function(callback)
+    {
+        config.setEmpty('image', 'postgres:9-alpine');
+        config.setEmpty('folder', config.getDataDir());
+        config.setEmpty('user', 'postgres');
+        config.setEmpty('password', 'password');
+        config.setEmpty('port', 5432);
+
+        //config.copyToUserDir(path.join(__dirname, 'postgres', 'conf'));
+        //config.copyToDataDir(path.join(__dirname, 'postgres', 'data'));
+
+        callback(null, 'setup loaded for ' + SCRIPTNAME);
+    },
+
+    debugcli(callback) {
+        let result = usdocker.outputRaw('cli', getContainerDef());
+        callback(result);
+    },
+
+    debugapi(callback) {
+        let result = usdocker.outputRaw('api', getContainerDef());
+        callback(result);
+    },
+
+    up: function(callback)
+    {
+        usdocker.up(CONTAINERNAME, getContainerDef(), callback);
+    },
+
+    status: function(callback) {
+        usdocker.status(CONTAINERNAME, callback);
+    },
+
+    down: function(callback)
+    {
+        usdocker.down(CONTAINERNAME, callback);
+    },
+
+    restart: function(callback)
+    {
+        usdocker.restart(CONTAINERNAME, getContainerDef(), callback);
+    },
+
+    client: function(callback, extraArgs)
+    {
+        usdocker.exec(CONTAINERNAME, ['psql'].concat(extraArgs), callback);
+    },
+
+    connect: function(callback, extraArgs)
+    {
+        usdocker.exec(CONTAINERNAME, ['bash'].concat(extraArgs), callback);
+    }
+};
